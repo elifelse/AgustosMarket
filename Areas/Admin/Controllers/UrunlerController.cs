1e96edc88c4bd19374c26c432d51a6ba489f2785
elifelse <elifgsavukcam@gmail.com>
2021-08-19 02:46:43 +0300

    Yönetim Paneli  V.2

---
+++ Areas/Admin/Controllers/UrunlerController.cs
@@ -0,0 +1,56 @@
+﻿using AgustosMarket.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgustosMarket.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UrunlerController : Controller
+    {
+        private readonly UygulamaDbContext _context;
+
+        public UrunlerController(UygulamaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Yeni()
+        {
+            ViewBag.Kategoriler = _context.Kategoriler.Select(k => new SelectListItem()
+            {
+                Text = k.KategoriAd,
+                Value = k.Id.ToString()
+            }).ToList();
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Yeni(Urun urun)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(urun);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Kategoriler = _context.Kategoriler.Select(k => new SelectListItem()
+            {
+                Text = k.KategoriAd,
+                Value = k.Id.ToString()
+            }).ToList();
+
+            return View();
+        }
+    }
+}
